2d87218d49391e771b6b20389e6f046619b377db
arun.rade <arun.rade@gmail.com>
2025-01-04 21:50:56 +0530

    hotel - java class

---
+++ src/main/java/com/example/bookings_ms/hotel.java
@@ -0,0 +1,10 @@
+package com.example.bookings_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotel {
+    @GetMapping("/hotel")
+    public String getData() {return  "Please book your hotel in New Delhi 20% discount" ; }
+}
