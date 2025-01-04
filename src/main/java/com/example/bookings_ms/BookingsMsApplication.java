77f34b6dd00c4f752ee27d5f570f0de51c28433b
arun.rade <arun.rade@gmail.com>
2025-01-04 21:12:32 +0530

    code-factor

---
+++ src/main/java/com/example/bookings_ms/BookingsMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookings_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingsMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookings_ms/BookingsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookings_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
